d2944af1d0bc4f6486385592bb90e2e8f03d692a
SinghRajenM <singh.rajen15@gmail.com>
2022-09-13 09:34:15 +0530

    Remove unused code

--- NppJSONViewer/NppJsonViewer/JSONDialog.h
+++ NppJSONViewer/NppJsonViewer/JSONDialog.h
@@ -23,7 +23,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "PluginInterface.h"
 #include <commctrl.h>
 #include "resource.h"
-#include "json.h"
 
 class JSONDialog : public DockingDlgInterface
 {
